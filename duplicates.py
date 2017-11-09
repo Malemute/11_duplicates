709d34fe1220e8601b1ec3036de0ab543fc46f2c
Malemute <eugeny.mamontov@gmail.com>
2017-11-09 13:47:39 +0300

    First draft

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,14 @@
+import sys
+import os
+from collections import Counter
+
 
 if __name__ == '__main__':
-    pass
+
+    if len(sys.argv) == 1:
+        sys.exit('{} <filename>'.format(sys.argv[0]))
+
+    dirpath = sys.argv[1]
+    for top, dirs, files in os.walk(dirpath):
+        for nm in files:
+            print(os.path.join(top, nm))
