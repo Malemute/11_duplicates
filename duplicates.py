5ef8daec029409ee72f4e1f965081410b0cffc6f
Malemute <eugeny.mamontov@gmail.com>
2017-11-09 15:52:50 +0300

    Completed module

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,29 @@
 import sys
 import os
+from os.path import join, getsize
 from collections import Counter
+
+
+def get_files_list(dir_path):
+
+    files_list = []
+
+    for top, dirs, files in os.walk(dirpath):
+        for the_name in files:
+            full_filename = join(top, the_name)
+            file_size = getsize(full_filename)
+            files_list.append((the_name, file_size))
+
+    return files_list
+
+
+def get_duplicates(files_list):
+
+    counted_files = Counter(files_list)
+
+    duplicates = [file_counter[0] for file_counter in counted_files if file_counter[1] > 1]
+
+    return duplicates
 
 
 if __name__ == '__main__':
@@ -9,6 +32,9 @@ if __name__ == '__main__':
         sys.exit('{} <filename>'.format(sys.argv[0]))
 
     dirpath = sys.argv[1]
-    for top, dirs, files in os.walk(dirpath):
-        for nm in files:
-            print(os.path.join(top, nm))
+    files_list = get_files_list(dirpath)
+
+    duplicates_list = get_duplicates(files_list)
+
+    for file, size in duplicates_list:
+        print('{}   {}'.format(file, size))
